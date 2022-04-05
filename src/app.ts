5d51ace2abd1f39dc60969eb585b424902cc8d20
hoangcuong2802 <65937145+hoangcuong2802@users.noreply.github.com>
2022-04-05 14:19:01 +0700

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,16 @@
 import * as MRE from "@microsoft/mixed-reality-extension-sdk";
 
-import WhiteBoard from './modules/whiteboard'
 import SignupForm from "./modules/signupform";
 
 export default class Continuum {
   private assets: MRE.AssetContainer;
-  private _whiteBoard: WhiteBoard;
   private _signupForm: SignupForm;
 
   constructor(private context: MRE.Context, private params: MRE.ParameterSet) {
-    this._whiteBoard = new WhiteBoard(context);
     this._signupForm = new SignupForm(context);
 
     this.context.onStarted(() => {
       switch (this.params.module) {
-        case "whiteBoard":
-          this._whiteBoard.started();
-          break;
         case "signupForm":
           this._signupForm.started();
           break;  
